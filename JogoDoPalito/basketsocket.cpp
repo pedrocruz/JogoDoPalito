750cf66a8835d15d9d4a2845005045882b84fb75
Pedro Cruz <pedrohccaminha@gmail.com>
2012-07-05 16:06:52 -0300

    messageReceived

--- JogoDoPalito/basketsocket.cpp
+++ JogoDoPalito/basketsocket.cpp
@@ -65,6 +65,16 @@ void BasketSocket::parseMessage(QString message)
 
 void BasketSocket::sendMessage(QString messageToSend)
 {
+    QByteArray block;
+    QDataStream out(&block, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_4_0);
+    out << (quint16)0;
+    out<< messageToSend;
+    out.device()->seek(0);
+    out << (quint16)(block.size() - sizeof(quint16));
+
+    this->tcpSocket->write(block);
+    this->tcpSocket->flush();
 }
 
 void BasketSocket::receiveResults()
